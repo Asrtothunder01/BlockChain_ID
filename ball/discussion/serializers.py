cda79cf62ac2f9f8db62964eebf815933f41d02f
Olorunfemi Daniel <163652660+Asrtothunder01@users.noreply.github.com>
2024-10-21 06:00:40 +0100

    Delete ball directory

--- ball/discussion/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Discussion
-
-class DiscussionSerializer(serializers.ModelSerializer):
-	user = UserSerializer (many = True)
-	class Meta:
-		model = Discussion
-		fields = ['id','user','title', 'body']
-		read_only_fields = ['id','user']
--- ball/upvote/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Upvote
-# Register your models here.
-admin.site.register(Upvote)
