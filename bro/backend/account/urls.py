67070f90abc9e78928a193aee8a32d67245117c1
Olorunfemi Daniel <163652660+Asrtothunder01@users.noreply.github.com>
2024-10-21 06:01:16 +0100

    Delete bro/backend directory

--- bro/backend/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-  path('list/', views.UserListView.as_view(), name = 'User_list'),
-  
-  path('create/', views.UserCreateView.as_view(),name = 'user_create'),
-]
